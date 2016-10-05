71e3de4a18085be5fa3630a8a9cefa031d66fe0e
Pitos <pitos007@gmail.com>
2016-10-05 11:34:18 +0100

    stable pre

--- src/xlsxtocsv/Update.java
+++ src/xlsxtocsv/Update.java
@@ -20,13 +20,13 @@ public class Update {
         TemplateWriter tempWrit = new TemplateWriter();
         tempWrit.createTemplateFile();
         
-        PriceListUpdater pr = new PriceListUpdater();
-        pr.readAndUpdatePriceFile();
-        Report rep = new Report();
-        rep.updateOutFile(pr.getPriceChangesList());
-        
-        PriceListWriter plw = new PriceListWriter();
-        plw.writeUpdatedFile(pr.getPriceFileList());
+//        PriceListUpdater pr = new PriceListUpdater();
+//        pr.readAndUpdatePriceFile();
+//        Report rep = new Report();
+//        rep.updateOutFile(pr.getPriceChangesList());
+//        
+//        PriceListWriter plw = new PriceListWriter();
+//        plw.writeUpdatedFile(pr.getPriceFileList());
         
     }
 }
